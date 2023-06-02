a5a3c3340ac1e4c8476b6550a3c940ad94df07f7
ChrisCheng <762968511@qq.com>
2023-06-02 09:20:37 +0800

    synthesis_style

--- MainWindow.py
+++ MainWindow.py
@@ -9,12 +9,12 @@ from Info import *
 from Rule import *
 
 
-class MainWindow(object):
+class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Main")
         Form.resize(500, 400)
-        self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
         self.label_2 = QtWidgets.QLabel(parent=Form)
@@ -27,9 +27,6 @@ class MainWindow(object):
         self.gridLayout.addWidget(
             self.label, 0, 0, 1, 1, QtCore.Qt.AlignmentFlag.AlignHCenter
         )
-        self.pushButton_2 = QtWidgets.QPushButton(parent=Form)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 9, 1, 1, 1)
         self.Chosen = QtWidgets.QListView(parent=Form)
         sizePolicy = QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Policy.Expanding,
@@ -44,9 +41,9 @@ class MainWindow(object):
         self.Fact = QtWidgets.QListView(parent=Form)
         self.Fact.setObjectName("Fact")
         self.gridLayout.addWidget(self.Fact, 2, 0, 1, 1)
-        self.pushButton = QtWidgets.QPushButton(parent=Form)
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 9, 0, 1, 1)
+        self.AnotherListView = QtWidgets.QListView(parent=Form)
+        self.AnotherListView.setObjectName("AnotherListView")
+        self.gridLayout.addWidget(self.AnotherListView, 2, 2, 1, 1)
         self.pushButton_3 = QtWidgets.QPushButton(parent=Form)
         sizePolicy = QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Ignored
@@ -67,17 +64,43 @@ class MainWindow(object):
         self.pushButton_4.setSizePolicy(sizePolicy)
         self.pushButton_4.setObjectName("pushButton_4")
         self.gridLayout.addWidget(self.pushButton_4, 3, 1, 1, 1)
-        self.horizontalLayout.addLayout(self.gridLayout)
-        self.FactList = []
-        # 编辑事实
-        self.pushButton_3.clicked.connect(self.Open_1)
-        # 管理规则
-        self.pushButton.clicked.connect(self.Open_2)
-        # 开始推理
-        self.pushButton_2.clicked.connect(self.Open_3)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 3, 2, 1, 1)
+        self.label_3 = QtWidgets.QLabel(parent=Form)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(
+            self.label_3, 0, 2, 1, 1, QtCore.Qt.AlignmentFlag.AlignHCenter
+        )
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.pushButton_2 = QtWidgets.QPushButton(parent=Form)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Main"))
+        self.label_2.setText(_translate("Form", "已选事实"))
+        self.label.setText(_translate("Form", "已有事实"))
+        self.pushButton_2.setText(_translate("Form", "开始推理"))
+        self.pushButton.setText(_translate("Form", "管理规则"))
+        self.pushButton_3.setText(_translate("Form", "编辑事实"))
+        self.pushButton_4.setText(_translate("Form", "选择事实"))
+        self.label_3.setText(_translate("Form", "已有规则"))
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.pushButton_3.clicked.connect(self.Open_1)
+        self.ui.pushButton.clicked.connect(self.Open_2)
+        self.ui.pushButton_2.clicked.connect(self.Open_3)
+        self.FactList = []
 
     def Open_3(self):
         self.form3 = QtWidgets.QDialog()
@@ -96,33 +119,20 @@ class MainWindow(object):
         self.ui1 = FactWindow()
         self.ui1.setupUi(self.form1)
         self.ui1.set_fact_list(self.FactList)
-        self.ui1.factSubmitted.connect(self.factSubmittedHandler)  # 连接信号和槽函数
+        self.ui1.factSubmitted.connect(self.factSubmittedHandler)
         self.form1.exec()
 
     def factSubmittedHandler(self, facts):
         self.FactList = facts
-        slm = QStringListModel()
+        slm = QtCore.QStringListModel()
         slm.setStringList(facts)
-        self.Fact.setModel(slm)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Main"))
-        self.label_2.setText(_translate("Form", "已选事实"))
-        self.label.setText(_translate("Form", "已有事实"))
-        self.pushButton_2.setText(_translate("Form", "开始推理"))
-        self.pushButton.setText(_translate("Form", "管理规则"))
-        self.pushButton_3.setText(_translate("Form", "编辑事实"))
-        self.pushButton_4.setText(_translate("Form", "选择事实"))
+        self.ui.Fact.setModel(slm)
 
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = MainWindow()
-    ui.setupUi(Form)
-
-    Form.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
